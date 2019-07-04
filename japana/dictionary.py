40753a3eac770b13dbe02d5a1a1df8e3af9b4e12
reem-codes <reem_brain@hotmail.com>
2019-07-04 23:29:45 +0300

    changed jamdict to make it faster

--- japana/dictionary.py
+++ japana/dictionary.py
@@ -1,10 +1,10 @@
-from jamdict import Jamdict
-jmd = Jamdict()
+from jamdict import Jamdict, config
+jmd = Jamdict(db_file=config.get_file('JAMDICT_DB'))
 
 
 def lookup_dic(word):
     word_dic = dict()
-    result = jmd.lookup(word)
+    result = jmd.lookup(word, strict_lookup=True, lookup_chars=False)
     result = result.to_json()['entries']
     for entry in result:
         if entry['kanji'][0]['text'] == word:
@@ -19,9 +19,9 @@ def lookup_dic(word):
                     if i >= 5:
                         break
                     word_dic['meaning'] += one_sense['text']
-                    if i != len(meaning['SenseGloss']) and i != 5:
+                    if i != len(meaning['SenseGloss']) and i != 4:
                         word_dic['meaning'] += "; "
-                if j != len(entry['senses']) and j != 3:
+                if j != len(entry['senses']) and j != 2:
                     word_dic['meaning'] += "<br>"
             return word_dic
     return {"word": word}
